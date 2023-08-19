a13d1a2614fa6aea6cd4a774f38283745931cfbf
Basil <basilreji3@gmail.com>
2023-08-19 15:04:03 +0530

    Function arguments

--- src/Main.hx
+++ src/Main.hx
@@ -1,25 +1,13 @@
-import Math.PI;
+function createMessage(?name:String):String { // Name is optional with ?
 
-class Main {
-	static function calcAreaOfCircle(radius:Float):Float {
-		return PI * radius * radius;
-	}
-
-	static function main() {
-		var doorOpen = true;
-
-		if (doorOpen) {
-			Sys.print('Door is open!');
-		} else if (!doorOpen) {
-			Sys.print('Door is closed dude!');
-		}
-
-		Sys.print('\nCome back later!!\n');
-
-		var area = calcAreaOfCircle(10);
-		trace(PI * 10 * 10);
-		Sys.print('Area is $area'); // Single quotes to use variables in string.
-	}
+	if (name == null)
+		return 'Hi, dude. Tell me your name!!';
+	else
+		return "Hello, " + name;
 }
-// cmd+shift+b to build py & js files, need to add those targets inside the hxml file.
-// use debugger for compiling without those files. f5
+
+function main() {
+	Sys.print(createMessage('Basil'));
+	Sys.print('\n');
+	Sys.print(createMessage());
+}
