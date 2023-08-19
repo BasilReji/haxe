634fa71f47e6f5dc32a8f98ac862138c3ff61cd4
Basil <basilreji3@gmail.com>
2023-08-19 14:58:18 +0530

    functions

--- src/Main.hx
+++ src/Main.hx
@@ -1,28 +1,24 @@
+import Math.PI;
+
 class Main {
+	static function calcAreaOfCircle(radius:Float):Float {
+		return PI * radius * radius;
+	}
+
 	static function main() {
-		var name = 'Basil Reji';
-		trace(name);
-		Sys.print(name);
-		var address = 'Basil Reji \n Panthanamolel house \n North Mazhuvannoor P.O.';
-		trace(address);
-		var question = 'Enter your name';
+		var doorOpen = true;
 
-		Sys.println(question);
-		// var consoleInput = Sys.stdin(); // Take value from console.
-		// name = consoleInput.readLine();
-		// trace(consoleInput);
+		if (doorOpen) {
+			Sys.print('Door is open!');
+		} else if (!doorOpen) {
+			Sys.print('Door is closed dude!');
+		}
 
-		var doorOpen:Bool = true;
-		trace(doorOpen);
+		Sys.print('\nCome back later!!\n');
 
-		var name1 = 'basil';
-		var name2 = 'reji';
-
-		var nameSame = name1 == name2;
-
-		trace(nameSame);
-
-		
+		var area = calcAreaOfCircle(10);
+		trace(PI * 10 * 10);
+		Sys.print('Area is $area'); // Single quotes to use variables in string.
 	}
 }
 // cmd+shift+b to build py & js files, need to add those targets inside the hxml file.
